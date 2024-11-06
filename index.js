d8482ab6866b916648a91fdcf57fba986e96431c
Nick Pruitt <pruitt.nick@gmail.com>
2024-11-06 13:42:01 -0600

    Update to latest stylelint
    Remove deprecated properties and update peerDependencies.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 module.exports = {
+  "extends": "stylelint-config-standard-scss",
+  "ignoreFiles": ["**/*.css", "**/kss"],
   "plugins": [
     "stylelint-order",
     "stylelint-scss"
   ],
-  "ignoreFiles": ["**/*.css", "**/kss"],
   "rules": {
     "at-rule-no-unknown": null,
-    "block-closing-brace-empty-line-before": "never",
-    "block-closing-brace-newline-before": "always-multi-line",
-    "block-opening-brace-newline-after": "always-multi-line",
-    "block-opening-brace-space-before": "always",
     "block-no-empty": true,
-    "color-hex-case": "upper",
     "color-hex-length": "short",
     "color-named": ["never", {
         "ignore": ["inside-function"]
@@ -19,56 +15,26 @@ module.exports = {
     "color-no-invalid-hex": true,
     "comment-no-empty": true,
     "comment-whitespace-inside": "always",
+    "custom-property-empty-line-before": null,
     "declaration-block-no-duplicate-properties": [true, {
         "ignore": ["consecutive-duplicates"]
     }],
-    "declaration-colon-space-after": "always-single-line",
-    "declaration-colon-space-before": "never",
-    "declaration-block-semicolon-newline-after": "always-multi-line",
-    "declaration-block-semicolon-space-before": "never",
     "declaration-block-single-line-max-declarations": 1,
-    "declaration-block-trailing-semicolon": "always",
     "font-family-no-duplicate-names": true,
     "font-family-no-missing-generic-family-keyword": true,
     "font-family-name-quotes": "always-where-recommended",
     "function-calc-no-unspaced-operator": true,
-    "function-comma-newline-after": "always-multi-line",
-    "function-comma-space-after": "always-single-line",
-    "function-comma-space-before": "never",
     "function-linear-gradient-no-nonstandard-direction": true,
     "function-name-case": "lower",
-    "function-parentheses-newline-inside": "always-multi-line",
-    "indentation": [4, {
-        "except": ["value"],
-        "indentInsideParens": "once-at-root-twice-in-block",
-        "severity": "error"
-    }],
     "keyframe-declaration-no-important": true,
     "length-zero-no-unit": true,
-    "max-empty-lines": 4,
-    "max-line-length": 1000,
     "max-nesting-depth": [0, {
         "ignoreAtRules": ["media", "supports", "include", "each", "if", "else"]
     }],
-    "media-feature-colon-space-after": "always",
-    "media-feature-colon-space-before": "never",
-    "media-feature-name-case": "lower",
-    "media-feature-name-no-unknown":[true, {
-      "ignoreMediaFeatureNames": ["query"]
-    }],
-    "media-feature-parentheses-space-inside": "never",
-    "media-feature-range-operator-space-after": "always",
-    "media-feature-range-operator-space-before": "always",
-    "no-descending-specificity": true,
+    "media-feature-name-no-unknown": true,
     "no-duplicate-selectors": true,
-    "no-eol-whitespace": true,
-    "no-extra-semicolons": true,
-    "no-missing-end-of-source-newline": true,
-    "number-leading-zero": "always",
-    "number-no-trailing-zeros": true,
     "number-max-precision": 4,
     "order/properties-alphabetical-order": true,
-    "property-case": "lower",
     "property-no-unknown": true,
     "rule-empty-line-before": ["always", {
         "except": ["after-single-line-comment", "first-nested"]
@@ -80,16 +46,17 @@ module.exports = {
     "scss/at-function-parentheses-space-before": "never",
     "scss/at-if-closing-brace-newline-after": "always-last-in-chain",
     "scss/at-if-closing-brace-space-after": "always-intermediate",
-    "scss/at-import-no-partial-leading-underscore": true,
     "scss/at-mixin-argumentless-call-parentheses": "never",
     "scss/at-mixin-parentheses-space-before": "never",
+    "scss/at-rule-conditional-no-parentheses": null,
     "scss/at-rule-no-unknown": true,
+    "scss/comment-no-empty": null,
     "scss/dollar-variable-colon-space-after": "always",
     "scss/dollar-variable-colon-space-before": "never",
     "scss/dollar-variable-no-missing-interpolation": true,
     "scss/double-slash-comment-empty-line-before": ["always", {
         "except": ["first-nested"],
-        "ignore": ["between-comments", "inside-block"]
+        "ignore": ["between-comments"]
     }],
     "scss/double-slash-comment-whitespace-inside": "always",
     "scss/operator-no-newline-after": true,
@@ -97,27 +64,16 @@ module.exports = {
     "scss/operator-no-unspaced": true,
     "selector-class-pattern": "^[a-zA-Z]+([a-zA-Z0-9-]*(__)?)*[a-zA-Z0-9]$",
     "selector-max-attribute": 2,
-    "selector-max-class": 2,
-    "selector-attribute-brackets-space-inside": "never",
-    "selector-combinator-space-after": "always",
-    "selector-combinator-space-before": "always",
-    "selector-descendant-combinator-no-non-space": true,
-    "selector-list-comma-newline-after": "always",
-    "selector-list-comma-newline-before": "never-multi-line",
-    "selector-pseudo-class-case": "lower",
+    "selector-max-class": 3,
     "selector-pseudo-class-no-unknown": true,
-    "selector-pseudo-element-case": "lower",
     "selector-pseudo-element-colon-notation": "double",
     "selector-pseudo-element-no-unknown": true,
     "selector-type-case": "lower",
     "selector-type-no-unknown": true,
     "shorthand-property-no-redundant-values": true,
     "string-no-newline": true,
-    "string-quotes": "single",
-    "unit-case": "lower",
     "unit-no-unknown": true,
     "value-keyword-case": ["lower", {
-        "ignoreProperties": ["/^\\$.*$/", 'text-rendering']
+        "ignoreProperties": ["/^\\$.*$/"]
     }]
-  }
 }
